a16146a22514848daeba10e1497e8ef21a44ff6b
Felipe Aravena <felipearavena98@hotmail.com>
2022-07-05 15:50:30 -0400

    Update proyects with new proyects

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -10,12 +10,127 @@ import restserver from '../../images/front-tech/restserver.jpg'
 import postman from '../../../rest-server.json'
 import './Gallery.scss'
 import './Backend.scss'
-
+import './Frontend.scss'
 
 const Gallery = () => {
     return (
         <>
+
+            <section className='section1'>
+                <h2 className='text-center text-white mt-5'>React apps</h2>
+                <div className='container-section'>
+                    <div class="containerfather mb-5 mt-5">
+                        <div class="card">
+                            <div class="card-header">
+                                <img src={buscarImgR} alt="rover" />
+                            </div>
+                            <div class="card-body">
+                                <span class="tag tag-teal">Formik</span>
+                                <h4>
+                                    Buscador de Imagenes con React, Formik y Fetch Api.
+                                </h4>
+                                <p>
+                                    Puedes buscar tus imagenes y la cantidad mediante los filtros.
+                                </p>
+                                <div className=''>
+                                    <button type="button" className="btn btn-success">Ver Proyecto</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header">
+                                <img src={giftApp} alt="ballons" />
+                            </div>
+                            <div class="card-body">
+                                <span class="tag tag-purple">Fetch</span>
+                                <h4>
+                                    Buscador de Gifs con React y Fetch Api.
+                                </h4>
+                                <p>
+                                    Aquí puedes encontrar muchos gifs de cualquier tipo, estos gifs provienen de la api de giphy.
+                                </p>
+                                <div className=''>
+                                    <button type="button" className="btn btn-success">Ver Proyecto</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header">
+                                <img src={formik} />
+                            </div>
+                            <div class="card-body">
+                                <span class="tag tag-pink">Formik</span>
+                                <h4>
+                                    Formulario de prueba de todo tipo de validaciones con Formik.
+                                </h4>
+                                <p>
+                                    El formulario es el resultado de una serie de pruebas, para ver el funcionamiento de Formik.
+                                </p>
+                                <div className=''>
+                                    <button type="button" className="btn btn-success">Ver Proyecto</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header">
+                                <img src={hooks} />
+                            </div>
+                            <div class="card-body">
+                                <span class="tag tag-pink">Bootstrap</span>
+                                <h4>
+                                    La siguiente lista de tareas o To do list funciona con hooks.
+                                </h4>
+                                <p>
+                                    El resultado de esta lista es para probar el funcionamiento de los hooks.
+                                </p>
+                                <div className=''>
+                                    <button type="button" className="btn btn-success">Ver Proyecto</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header">
+                                <img src={gestor} />
+                            </div>
+                            <div class="card-body">
+                                <span class="tag tag-pink">Formik</span>
+                                <h4>
+                                    Formulario de prueba con validaciones de Formik.
+                                </h4>
+                                <p>
+                                    Este formulario es el resultado de algunas pruebas de formik.
+                                </p>
+                                <div className=''>
+                                    <button type="button" className="btn btn-success">Ver Proyecto</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header">
+                                <img src={lista} />
+                            </div>
+                            <div class="card-body">
+                                <span class="tag tag-pink">JavaScript</span>
+                                <h4>
+                                    Lista de tareas o To do list, creada en base a React.
+                                </h4>
+                                <p>
+                                    Este To do list, funciona con JavaScript y LocalStorage.
+                                </p>
+                                <div className=''>
+                                    <button type="button" className="btn btn-success">Ver Proyecto</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+
+
             <section className='backbody'>
+                <h2 className='text-center text-white mt-5'>Backend Api</h2>
                 <div className='container py-4'>
 
                     <div className="postcard dark red">
